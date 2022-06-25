7052a8716eb0691badde925c6184774da7455602
HaveNoLimit <93959590+HaveNoLimit@users.noreply.github.com>
2022-06-25 14:28:01 -0400

    header component created

---
+++ star-wars/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+
+const api = "https://swapi.dev/api/starships/";
+
+function Header() {
+  return (
+    <>
+      <h1>Starship Boutique</h1>
+    </>
+  );
+}
+
+export default Header;
