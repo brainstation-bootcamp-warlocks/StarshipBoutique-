1ee70878f2b5b4d389f4263d826b6995b02f3c0f
JumaMac <mjustinmacaraig@gmail.com>
2022-06-25 14:43:07 -0400

    added basic cart

---
+++ star-wars/src/components/Cart/Cart.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Cart.css";
+
+export default function Cart(props) {
+    return (
+        <div className="cart-wrapper">
+            <i className="fas fa-shopping-cart"></i>
+            <span className="quantity">{props.quantity}</span>
+            <p>Cart</p>
+        </div>
+    );
+}
